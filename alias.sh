c03b08a21455df5b0f98836ebf1e85922e1ac38d
Zach Bray <zachbray@googlemail.com>
2016-12-01 19:31:47 +0000

    Replaced aliases with more powerful functions.

--- alias.sh
+++ alias.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
-shopt -s expand_aliases
 
-DOCKER_PREFIX='docker run -it -v $(pwd):/home/dev/src -v ~/.ssh:/home/dev/.ssh -v ~/.gitconfig:/home/dev/.gitconfig'
-alias vim.base="$DOCKER_PREFIX env/vim/base"
-alias vim.js="$DOCKER_PREFIX env/vim/js"
-alias vim.ts="$DOCKER_PREFIX env/vim/ts"
-alias vim.ts-rc="$DOCKER_PREFIX env/vim/ts-rc"
-alias vim.ts2="echo Use vim.ts instead"
-alias vim.mono="$DOCKER_PREFIX env/vim/mono"
-alias vim.rust="$DOCKER_PREFIX  -v ~/.cargo:/home/dev/.cargo env/vim/rust"
-alias vim.rust-nightly="$DOCKER_PREFIX -v ~/.cargo:/home/dev/.cargo env/vim/rust-nightly"
-alias vim.eclim="$DOCKER_PREFIX -v ~/.gradle:/home/dev/.gradle env/vim/eclim"
-alias vim.pony="$DOCKER_PREFIX env/vim/pony"
-alias vim.ocaml="$DOCKER_PREFIX env/vim/ocaml"
+function start_vim() {
+  if  [ $# -eq 1 ]
+  then
+    echo Starting vim for configuration $1
+    docker run -it -v $(pwd):/home/dev/src \
+                   -v ~/.ssh:/home/dev/.ssh \
+                   -v ~/.gitconfig:/home/dev/.gitconfig \
+                   -v ~/.gradle:/home/dev/.gradle \
+                   -v ~/.cargo:/home/dev/.cargo \
+                   -v ~/.m2:/home/dev/.m2 \
+                   env/vim/$1
+  else
+    echo Must supply one and only one configuration argument: base, js, ts, ts-rc, mono, rust, rust-nightly, eclim, pony or ocaml
+  fi
+}
+
+function start_container() {
+  EXPOSE_ARG=""
+  if [ $# -eq 0 ]
+  then
+    echo Must supply at least one configuration argument, eg., start_container js
+  else
+    echo Starting docker for language $1 and exposing port $2
+    docker run -it -v $(pwd):/home/dev/src \
+                   -v ~/.ssh:/home/dev/.ssh \
+                   -v ~/.gitconfig:/home/dev/.gitconfig \
+                   -v ~/.gradle:/home/dev/.gradle \
+                   -v ~/.cargo:/home/dev/.cargo \
+                   -v ~/.m2:/home/dev/.m2 \
+                   --expose $2 -p $2:$2 \
+                   --entrypoint /bin/bash \
+                   env/vim/$1
+  fi
+}

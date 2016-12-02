7185ca24169dd4eb06327c3fac6dfa9cd2040a70
Zach Bray <zachbray@googlemail.com>
2016-12-02 15:29:34 +0000

    Improved the ability to expose ports on the development environment containers.

--- utils.sh
+++ utils.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
 function start_vim() {
-  if  [ $# -eq 1 ]
-  then
-    echo Starting vim for configuration $1
-    docker run -it -v $(pwd):/home/dev/src \
-                   -v ~/.ssh:/home/dev/.ssh \
-                   -v ~/.gitconfig:/home/dev/.gitconfig \
-                   -v ~/.gradle:/home/dev/.gradle \
-                   -v ~/.cargo:/home/dev/.cargo \
-                   -v ~/.m2:/home/dev/.m2 \
-                   env/vim/$1
-  else
-    echo Must supply one and only one configuration argument: base, js, ts, ts-rc, mono, rust, rust-nightly, eclim, pony or ocaml
-  fi
-}
-
-function start_container() {
-  EXPOSE_ARG=""
   if [ $# -eq 0 ]
   then
-    echo Must supply at least one configuration argument, eg., start_container js
+    echo ""
+    echo "Must supply at least one configuration argument and optionally ports to expose."
+    echo ""
+    echo "Format:"
+    echo "    start_vim <CONFIG> [<PORT_A> <PORT_B> ...]"
+    echo ""
+    echo "Where:"
+    echo "    CONFIG is a vim container configuration: base, js, ts, ts-rc, mono, rust, rust-nightly, eclim, pony or ocaml."
+    echo ""
+    echo "For example:"
+    echo "    start_vim ts 3000 8080"
+    echo ""
   else
-    echo Starting docker for language $1 and exposing port $2
+    echo Starting docker for language $1
+    EXPOSE_ARGS=""
+    ENTRYPOINT_ARGS=""
+    for i in ${@:2}
+    do
+        EXPOSE_ARGS="$EXPOSE_ARGS --expose $i -p $i:$i"
+        ENTRYPOINT_ARGS="--entrypoint /bin/bash"
+        echo Exposing port $i
+    done
     docker run -it -v $(pwd):/home/dev/src \
                    -v ~/.ssh:/home/dev/.ssh \
                    -v ~/.gitconfig:/home/dev/.gitconfig \
                    -v ~/.gradle:/home/dev/.gradle \
                    -v ~/.cargo:/home/dev/.cargo \
                    -v ~/.m2:/home/dev/.m2 \
-                   --expose $2 -p $2:$2 \
-                   --entrypoint /bin/bash \
+                   $EXPOSE_ARGS \
+                   $ENTRYPOINT_ARGS \
                    env/vim/$1
   fi
 }
